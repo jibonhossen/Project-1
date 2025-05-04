197ec7dfcf9255cbdc0aa4c9f79ea94befa001bf
jibonhossen <jibonhossen2026@gmail.com>
2025-05-04 21:54:08 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/app', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
